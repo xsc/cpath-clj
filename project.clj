f4f62933c34f854c8c4e58ffb409b39ceab673d1
Yannick Scherer <yannick.scherer@stylefruits.de>
2014-05-27 10:10:42 +0200

    Version 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cpath-clj "0.1.0-SNAPSHOT"
+(defproject cpath-clj "0.1.0"
   :description "Classpath Utilities for Clojure"
   :url "https://github.com/xsc/cpath-clj"
   :license {:name "Eclipse Public License"
