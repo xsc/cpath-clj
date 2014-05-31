d644cd5d29afd99692d73b57e7e0af6ec1946bef
Yannick Scherer <yannick.scherer@stylefruits.de>
2014-05-31 17:10:14 +0200

    add 'midje' dependency.

--- project.clj
+++ project.clj
@@ -4,4 +4,11 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/java.classpath "0.2.2"]])
+                 [org.clojure/java.classpath "0.2.2"]]
+  :profiles {:test {:dependencies [[midje "1.6.3" :exclusions [joda-time]]
+                                   [joda-time "2.2"]]
+                    :plugins [[lein-midje "3.1.3"]]
+                    :resource-paths ["test-resources"]}}
+  :aliases {"test" ["with-profile" "+test" "midje"]
+            "repl" ["with-profile" "+test" "repl"]}
+  :pedantic? :abort)
