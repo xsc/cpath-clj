c7f2dc834b7194b992fb00051b93ceb1e2759b1c
Yannick Scherer <yannick.scherer@stylefruits.de>
2015-04-16 18:44:37 +0200

    switch to MIT License.

--- project.clj
+++ project.clj
@@ -1,8 +1,10 @@
 (defproject cpath-clj "0.1.3-SNAPSHOT"
   :description "Classpath Utilities for Clojure"
   :url "https://github.com/xsc/cpath-clj"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "MIT License"
+            :url "http://opensource.org/licenses/MIT"
+            :year 2014
+            :key "mit"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/java.classpath "0.2.2"]]
   :profiles {:test {:dependencies [[midje "1.6.3" :exclusions [joda-time]]
